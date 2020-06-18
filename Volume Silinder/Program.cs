4c06d6cdca59ce0460b994157dcda3049b204b71
Sherly <sherly_mauren_25rpl@student.smktelkom-mlg.sch.id>
2020-06-18 08:15:30 +0700

    first commit

---
+++ Volume Silinder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Volume_Silinder
+{
+    class Program
+    {
+        static void Main()
+        {
+            double JARI, TINGGI, DIAMETER, LUASALAS, VOLUME;
+            Console.WriteLine("\n Menghitung Luas Silinder \n");
+            Console.Write("\n Jari - Jari       = ");
+            JARI = double.Parse(Console.ReadLine());
+            Console.Write(" Tinggi            = ");
+            TINGGI = double.Parse(Console.ReadLine());
+            DIAMETER = JARI * 2;
+            LUASALAS = JARI * JARI * 22 / 7;
+            VOLUME = LUASALAS * TINGGI;
+            Console.Write("\n Diameter          = " + DIAMETER);
+            Console.Write("\n Luas Alas         = " + LUASALAS);
+            Console.Write("\n Volume Silinder   = " + VOLUME);
+            Console.ReadKey();
+        }
+    }
+}
